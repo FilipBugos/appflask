e70706f40cb74f4b4242bdf4ea5e510c53cf2886
Matej Kleman <Matej_Kleman@Dell.com>
2023-03-12 15:09:04 +0100

    Update image to ubuntu:20.14

--- deploy.sh
+++ deploy.sh
@@ -2,7 +2,7 @@ apt-get update -y && \
     apt-get install -y python3-pip && \
     apt install -y libpq-dev
 
-pip3 install -r requirements.txt
+pip3 install -r app/requirements.txt
 
 export FLASK_APP=app/flask_app.py
 export FLASK_RUN_HOST=0.0.0.0
