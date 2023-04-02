0e4babd1d5069bdfec14d04ccab48ad452193997
Filip Bugos <91373231+FilipBugos@users.noreply.github.com>
2023-04-02 11:01:41 +0200

    fix: deployment

--- app.py
+++ app.py
@@ -1,4 +1,10 @@
-from flask import Flask, render_template, request
+import os
+from datetime import datetime
+
+from flask import Flask, redirect, render_template, request, send_from_directory, url_for
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf.csrf import CSRFProtect
 
 app = Flask(__name__)
 
--- config/config.py
+++ config/config.py
@@ -1,3 +1,5 @@
+import os
+
 conn_str = os.environ['AZURE_POSTGRESQL_CONNECTIONSTRING']
 conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
 
