157625efa8993c1abd37e267a7009d79258a6d10
Filip Bugos <91373231+FilipBugos@users.noreply.github.com>
2023-04-29 19:20:31 +0200

    fix: config

--- config/config.py
+++ config/config.py
@@ -12,10 +12,10 @@ import os
 
 
 conn_str = os.environ['AZURE_STORAGE_CONNECTIONSTRING']
-conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+conn_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(';')}
 
 end_str = os.environ['AZURE_STORAGE_ENDPOINTS']
-end_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in conn_str.split(' ')}
+end_str_params = {pair.split('=')[0]: pair.split('=')[1] for pair in end_str.split(';')}
 
 IS_EMULATED = False
 STORAGE_CONNECTION_STRING = 'DefaultEndpointsProtocol=https;AccountName={accountname};AccountKey={key};EndpointSuffix=core.windows.net;BlobEndpoint={blobE};TableEndpoint={tableE};QueueEndpoint={queeE};'.format(
